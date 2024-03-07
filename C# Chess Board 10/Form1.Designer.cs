e532e9f477e7febbd7e51fc13512cf771c48ee71
Kulpot <kulpotwahu@gmail.com>
2024-03-08 00:46:42 +0800

    comboBox

--- C# Chess Board 10/Form1.Designer.cs
+++ C# Chess Board 10/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // label1
@@ -40,11 +41,20 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(667, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,5 +66,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
--- C# Chess Board 10/Form1.cs
+++ C# Chess Board 10/Form1.cs
@@ -11,6 +11,7 @@ using System.Windows.Forms;
 //--------------------------- C# Chess Board 10 gui controls -------------------------------------------------
 //ref link:https://www.youtube.com/watch?v=cTNtnS6BSN0&list=PLhPyEFL5u-i0YDRW6FLMd1PavZp9RcYdF&index=10&t=1s
 
+// add label, comboBox,
 
 namespace C__Chess_Board_10
 {
