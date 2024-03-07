102c3441e3ebcede020c404b207bf70037e0753d
Kulpot <kulpotwahu@gmail.com>
2024-03-08 00:54:41 +0800

    label text

--- C# Chess Board 10/Form1.Designer.cs
+++ C# Chess Board 10/Form1.Designer.cs
@@ -38,14 +38,15 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(441, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Select a piece from the drop down menu and click any square i will show you all l" +
+    "egal moves";
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(394, 1);
+            this.comboBox1.Location = new System.Drawing.Point(461, 1);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 1;
@@ -61,7 +62,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(526, 531);
+            this.ClientSize = new System.Drawing.Size(594, 531);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label1);
